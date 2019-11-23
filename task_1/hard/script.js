e8695f8663cc22fd0ef8341adfafa188333e2e91
Ilya <klyukovskiy@gmail.com>
2019-11-23 21:49:19 +0300

    Complete difficult quest

--- task_1/hard/script.js
+++ task_1/hard/script.js
@@ -1,8 +1,10 @@
 let num = 33721;
+let numStr = num.toString();
 
 // Произведение цифр
-let compositionNum = 3*3*7*2*1;
+let compositionNum = numStr[0]*numStr[1]*numStr[2]*numStr[3]*numStr[4];
 console.log(compositionNum);
+
 
 // Возведение в степень по ES7
 let numDegreeEs7 = compositionNum**2;
