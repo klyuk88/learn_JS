b0ed86ec608790c4401ef6ef453ea352a1819923
Ilya <klyukovskiy@gmail.com>
2020-01-03 22:45:42 +0300

    modal task done

--- task_9/yoga/js/script.js
+++ task_9/yoga/js/script.js
@@ -98,8 +98,44 @@ window.addEventListener('DOMContentLoaded',function () {
 
     setClock('timer',deadline);
 
+
+
+//  модальное окно
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+more.addEventListener('click',function () {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click',function () {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+});
+
+
+let tabsBtn = document.querySelectorAll('.description-btn');
+
+function openModal(btn) {
+    btn.addEventListener('click',function () { 
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+     });
+}
+
+tabsBtn.forEach(element => {
+    openModal(element);
+});
+
+
+
+
+
  });
 
-//  4) Изменить скрипт так, чтобы в таком случае выводилось: 00:00:00 
 
-// Необходимо подставлять 0 перед значениями, которые состоят из одной цифры (из 4:6:50 сделает 04:06:50)
+
